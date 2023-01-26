e72f1eea536e1b378e894033c348901615f14631
Themstr <maminou.ayadi@icloud.com>
2023-01-26 09:43:19 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './Components/Form'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
